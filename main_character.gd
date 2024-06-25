800f72e95c8156a3f4ca40beb3ddd42f95bed2a7
Dmitry Okishev <dimok4a@mail.ru>
2024-06-25 18:55:56 +0400

    changed movement logic

--- main_character.gd
+++ main_character.gd
@@ -3,6 +3,25 @@ extends CharacterBody2D
 const speed = 100
 
 
+func render_animation(move_left, move_down, move_up, move_right):
+	var current_animation = "" 
+	var diagonal_movement = (move_down or move_up) and (move_left or move_right)
+	if (move_left):
+		current_animation = "leftwalk"
+	if (move_right):
+		current_animation = "rightwalk"
+	if (move_up && !diagonal_movement):
+		current_animation = "upwalk"
+	if (move_down && !diagonal_movement):
+		current_animation = "downwalk"
+	if (
+		not (move_down or move_left or move_right or move_up)
+		or (move_left and move_right) 
+		or (move_up and move_down)
+		):
+		current_animation = "idle"
+	$AnimatedSprite2D.play(current_animation)
+
 func _physics_process(delta):
 	var current_speed = speed
 	var move_left = false
@@ -10,46 +29,31 @@ func _physics_process(delta):
 	var move_up = false
 	var move_right = false
 	if Input.is_action_pressed("ui_right"):
-		#$AnimatedSprite2D.play("rightwalk")
-		#velocity.x = speed
 		move_right = true
-		#velocity.y = 0
 	if Input.is_action_pressed("ui_left"):
-		#$AnimatedSprite2D.play("leftwalk")
 		move_left = true
-		#velocity.x = -speed
-		#velocity.y = 0
 	if Input.is_action_pressed("ui_down"):
-		#$AnimatedSprite2D.play("downwalk")
 		move_down = true
-		#velocity.y = speed
-		#velocity.x = 0
 	if Input.is_action_pressed("ui_up"):
-		#$AnimatedSprite2D.play("upwalk")
-		#velocity.y = -speed
 		move_up = true
-		#velocity.x = 0
 	
 	var diagonal_movement = (move_down or move_up) and (move_left or move_right)
-	if (diagonal_movement):
-		current_speed = speed/2
-		
-	if (move_left):
-		velocity.x = -current_speed
-		$AnimatedSprite2D.play("leftwalk")
-	if (move_right):
-		velocity.x = current_speed
-		$AnimatedSprite2D.play("rightwalk")
-	if (move_up):
-		velocity.y = -current_speed
-		if (!diagonal_movement):
-			$AnimatedSprite2D.play("upwalk")
-	if (move_down):
-		velocity.y = current_speed
-		if (!diagonal_movement):
-			$AnimatedSprite2D.play("downwalk")
-	if (not (move_down or move_left or move_right or move_up)):
-		$AnimatedSprite2D.play("idle")
+
+	if (!diagonal_movement):
+		velocity.y = 0
+		velocity.x = 0
+	if (move_left): velocity.x = -1
+	if (move_right): velocity.x = 1
+	if (move_up): velocity.y = -1
+	if (move_down): velocity.y = 1
+	if (
+		not (move_down or move_left or move_right or move_up) 
+		or (move_left and move_right) 
+		or (move_up and move_down)
+		):
 		velocity.x = 0
 		velocity.y = 0
+	velocity = velocity.normalized()
+	velocity *= current_speed
+	render_animation(move_left, move_down, move_up, move_right)
 	move_and_slide()
